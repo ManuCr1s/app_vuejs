a3d7752a426b70ae4b505a6950a2deaffa81094f
mcristobal <mcristobaln@outlook.com>
2024-10-30 03:51:34 -0500

    feat: Modify index router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import DashboardLayout from "../views/Layout/DashboardLayout.vue";
+import ProfileLayout from "../views/Layout/ProfileLayout.vue";
+import DashboardLayoutVr from "../views/Layout/DashboardLayoutVr.vue";
+import PageLayout from "../views/Layout/PageLayout";
 import AuthBasicLayout from "../views/Layout/AuthBasicLayout";
 
+const Sales = () => import("../views/Dashboard/Sales.vue");
+const Automotive = () => import("../views/Dashboard/Automotive.vue");
+const Datatables = () =>
+  import(/* webpackChunkName: "pages" */ "@/views/Applications/Datatables.vue");
 const SignUpBasic = () =>
   import(
     "@/views/Pages/Authentication/SignUp/Basic.vue"
@@ -20,6 +28,30 @@ let authBasicPages = {
     ],
   };
 const routes = [
+    {
+      path: "/dashboard",
+      name: "Sales",
+      component: Sales,
+      meta: {
+        groupName: "Dashboards",
+      },
+    },
+    {
+      path: "/productor",
+      name: "Automotive",
+      component: Automotive,
+      meta: {
+        groupName: "Dashboards",
+      },
+    },
+    {
+      path: "/list",
+      name: "Datatables",
+      component: Datatables,
+      meta: {
+        groupName: "Applications",
+      },
+    },
     authBasicPages,
 ];
 const router = new VueRouter({
