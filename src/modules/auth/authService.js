6ba0323be5b79ded25b3cb01795792b2b137693a
mcristobal <mcristobaln@outlook.com>
2024-11-10 18:32:18 -0500

    feat: Withtoken in Axios

--- src/modules/auth/authService.js
+++ src/modules/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'http://apillama.local.com/';
 axios.defaults.withCredentials = true;
-
+axios.defaults.withXSRFToken = true;
 const login = (form) => {
     return axios.get('sanctum/csrf-cookie')
         .then(() => {
